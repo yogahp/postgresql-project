b6e5bd5a822e8d6cade4e9f3065f5ece99ce822d
Yoga Hapriana <thenelse.v2@gmail.com>
2023-12-08 17:41:14 +0700

    Add sql/007-FETCH-NEXT.sql

---
+++ sql/007-FETCH-NEXT.sql
@@ -0,0 +1,37 @@
+-- PostgreSQL FETCH examples
+SELECT
+    film_id,
+    title
+FROM
+    film
+ORDER BY
+    title 
+FETCH FIRST ROW ONLY;
+
+SELECT
+    film_id,
+    title
+FROM
+    film
+ORDER BY
+    title 
+FETCH FIRST 1 ROW ONLY;
+
+SELECT
+    film_id,
+    title
+FROM
+    film
+ORDER BY
+    title 
+FETCH FIRST 5 ROW ONLY;
+
+SELECT
+    film_id,
+    title
+FROM
+    film
+ORDER BY
+    title 
+OFFSET 5 ROWS 
+FETCH FIRST 5 ROW ONLY; 
