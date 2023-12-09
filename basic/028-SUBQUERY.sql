3259a62b5ce4246130cd0775bfdb5eeddc5a2622
Yoga Hapriana <thenelse.v2@gmail.com>
2023-12-09 13:26:37 +0700

    Add basic/028-SUBQUERY.sql

---
+++ basic/028-SUBQUERY.sql
@@ -0,0 +1,70 @@
+-- Introduction to PostgreSQL subquery
+SELECT
+	AVG (rental_rate)
+FROM
+	film;
+	
+SELECT
+	film_id,
+	title,
+	rental_rate
+FROM
+	film
+WHERE
+	rental_rate > 2.98;
+	
+SELECT
+	film_id,
+	title,
+	rental_rate
+FROM
+	film
+WHERE
+	rental_rate > (
+		SELECT
+			AVG (rental_rate)
+		FROM
+			film
+	);
+	
+SELECT
+	inventory.film_id
+FROM
+	rental
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+WHERE
+	return_date BETWEEN '2005-05-29'
+AND '2005-05-30';
+
+SELECT
+	film_id,
+	title
+FROM
+	film
+WHERE
+	film_id IN (
+		SELECT
+			inventory.film_id
+		FROM
+			rental
+		INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+		WHERE
+			return_date BETWEEN '2005-05-29'
+		AND '2005-05-30'
+	);
+	
+-- PostgreSQL subquery with EXISTS operator
+SELECT
+	first_name,
+	last_name
+FROM
+	customer
+WHERE
+	EXISTS (
+		SELECT
+			1
+		FROM
+			payment
+		WHERE
+			payment.customer_id = customer.customer_id
+	);
