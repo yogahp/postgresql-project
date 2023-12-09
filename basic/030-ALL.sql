94df5c99bec7e5ecca170b16538076c2cc170011
Yoga Hapriana <thenelse.v2@gmail.com>
2023-12-09 14:32:05 +0700

    Add basic/030-ALL.sql

---
+++ basic/030-ALL.sql
@@ -0,0 +1,27 @@
+-- PostgreSQL ALL operator examples
+SELECT
+    ROUND(AVG(length), 2) avg_length
+FROM
+    film
+GROUP BY
+    rating
+ORDER BY
+    avg_length DESC;
+    
+SELECT
+    film_id,
+    title,
+    length
+FROM
+    film
+WHERE
+    length > ALL (
+            SELECT
+                ROUND(AVG (length),2)
+            FROM
+                film
+            GROUP BY
+                rating
+    )
+ORDER BY
+    length;
