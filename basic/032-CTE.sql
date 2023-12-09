125a0b8602cb45a99c3f7cdf675aeaad28799347
Yoga Hapriana <thenelse.v2@gmail.com>
2023-12-09 15:02:13 +0700

    Add basic/032-CTE.sql

---
+++ basic/032-CTE.sql
@@ -0,0 +1,54 @@
+-- A simple PostgreSQL CTE example
+WITH cte_film AS (
+    SELECT 
+        film_id, 
+        title,
+        (CASE 
+            WHEN length < 30 THEN 'Short'
+            WHEN length < 90 THEN 'Medium'
+            ELSE 'Long'
+        END) length    
+    FROM
+        film
+)
+SELECT
+    film_id,
+    title,
+    length
+FROM 
+    cte_film
+WHERE
+    length = 'Long'
+ORDER BY 
+    title; 
+    
+-- Joining a CTE with a table example
+WITH cte_rental AS (
+    SELECT staff_id,
+        COUNT(rental_id) rental_count
+    FROM   rental
+    GROUP  BY staff_id
+)
+SELECT s.staff_id,
+    first_name,
+    last_name,
+    rental_count
+FROM staff s
+INNER JOIN cte_rental USING (staff_id); 
+
+-- The following statement illustrates how to use the CTE with the RANK() window function:
+WITH cte_film AS  (
+    SELECT film_id,
+        title,
+        rating,
+        length,
+        RANK() OVER (
+            PARTITION BY rating
+            ORDER BY length DESC) 
+        length_rank
+    FROM 
+        film
+)
+SELECT *
+FROM cte_film
+WHERE length_rank = 1;
